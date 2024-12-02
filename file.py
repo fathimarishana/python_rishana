5248daec67e2519f0c4ac3def692840ca52eee90
fathimarishana <fathimarishana835@gmail.com>
2024-12-02 16:14:53 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,8 @@
+file_name = input("enter the file name:")
+if "." in file_name:
+    extension = file_name.split(".")[-1]
+    print(f"the file extension is : {extension}")
+else:
+    print("the file has no extension.")
+~                                                                                                                                             
+~           
